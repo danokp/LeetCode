dc487890afb77e819216dc4872930dfd9352953e
Daniil Penkov <49104777+danokp@users.noreply.github.com>
2023-02-05 10:15:09 +0000

    Sync LeetCode submission - Roman to Integer (python3)

---
+++ leetcode_submissions/problems/roman_to_integer/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_to_int_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        s = s.replace('IV', 'IIII')
+        s = s.replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX')
+        s = s.replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC')
+        s = s.replace('CM', 'DCCCC')
+        total = 0
+        for ch in s:
+            total += roman_to_int_dict[ch]    
+        return total
