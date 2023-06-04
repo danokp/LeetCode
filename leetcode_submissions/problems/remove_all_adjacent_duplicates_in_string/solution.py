ce5b847947efe91c0405e53a411acca485bcdddf
Daniil Penkov <49104777+danokp@users.noreply.github.com>
2023-06-04 14:26:30 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (python3)

---
+++ leetcode_submissions/problems/remove_all_adjacent_duplicates_in_string/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for chr in s:
+            if stack and stack[-1] == chr:
+                stack.pop()
+            else:
+                stack.append(chr)
+        return ''.join(stack)
