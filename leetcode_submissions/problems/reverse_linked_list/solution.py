d3b83a47f32033e5dd04a8d71a55b4cec1b155ba
Daniil Penkov <49104777+danokp@users.noreply.github.com>
2023-06-11 16:56:54 +0000

    Sync LeetCode submission - Reverse Linked List (python3)

---
+++ leetcode_submissions/problems/reverse_linked_list/solution.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        prev = None
+        cur = head
+        while cur:
+            cur.next, cur, prev = prev, cur.next, cur
+        return prev
