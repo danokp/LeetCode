e1ead52cb8c302f7bd380d68eade168fb443b9d4
Daniil Penkov <49104777+danokp@users.noreply.github.com>
2023-06-22 09:02:23 +0000

    Sync LeetCode submission - Majority Element (python3)

--- leetcode_submissions/problems/majority_element/solution.py
+++ leetcode_submissions/problems/majority_element/solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-
-        num_dict = Counter(nums)
-        for num, count in num_dict.items():
-            if count > len(nums)/2:
-                return num
+        return Counter(nums).most_common(1)[0][0]
