a706c6a5c07241580f3f81188fd59019e322456e
Daniil Penkov <49104777+danokp@users.noreply.github.com>
2023-06-25 08:59:20 +0000

    Sync LeetCode submission - Coin Change (python3)

--- leetcode_submissions/problems/coin_change/solution.py
+++ leetcode_submissions/problems/coin_change/solution.py
@@ -1,16 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        @lru_cache(None)
-        def inner(amount):
-            if amount == 0:
-                return 0
-            if amount < 0:
-                return float('inf')
-            ret = float('inf')
-            for coin in coins:
-                ret = min(ret, inner(amount - coin) + 1)
-            return ret
-        ans = inner(amount)
+        dp = [float('inf') for _ in range(amount + 1)]
+        dp[0] = 0
 
-        return ans if ans != float('inf') else -1
+        for i in range(len(dp)):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        
+        return dp[-1] if dp[-1] < float('inf') else -1
